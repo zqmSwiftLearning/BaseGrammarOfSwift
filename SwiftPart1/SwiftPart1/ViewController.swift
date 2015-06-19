41ac5ed139b6f834e2bc65c41f276169d8e8bddb
ZQM <laznrbfe@163.com>
2015-06-19 17:11:33 +0800

    初始化第一个Swift工程

---
+++ SwiftPart1/SwiftPart1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftPart1
+//
+//  Created by laznrbfe on 15-6-19.
+//  Copyright (c) 2015年 QM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
